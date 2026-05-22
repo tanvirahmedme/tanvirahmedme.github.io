dee2a7bf646a06ccbbaf298c0cb2cc19fea56fac
tanvirahmedme <tanvir09k3@gmail.com>
2026-05-22 20:57:22 +0000

    Deploying to gh-pages from @ tanvirahmedme/tanvirahmedme.github.io@d46f306796a4cd2079e1c8571a6e916fb50c8315 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -521,20 +521,6 @@ ninja.data = [{
           window.open("mailto:%74%61%6E%76%69%72%61%68%6D%65%64%6D%63%6C@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
