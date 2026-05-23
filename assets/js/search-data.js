81b959f29bdd079fb0c354a734625b6cafee13ca
tanvirahmedme <tanvir09k3@gmail.com>
2026-05-23 20:14:05 +0000

    Deploying to gh-pages from @ tanvirahmedme/tanvirahmedme.github.io@8c3af269d0ae264deaf68b71a9d18845d34935c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-false",
-          title: "false",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
