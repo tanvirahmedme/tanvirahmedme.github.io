9644e67bddba6f4d120752c4d15157af81d201b5
tanvirahmedme <tanvir09k3@gmail.com>
2026-05-27 14:48:29 +0000

    Deploying to gh-pages from @ tanvirahmedme/tanvirahmedme.github.io@a31bb031124dbde7ffd9382af442acd281e9cb7e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,11 +425,6 @@ ninja.data = [{
           description: "A custom low-power wearable that predicts cattle behavior from motion and GPS data — designed end-to-end from PCB to a deployed real-time ML dashboard.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
