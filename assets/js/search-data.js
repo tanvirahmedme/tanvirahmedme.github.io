0d330574820969b465081cbd3d99f899e6dda3a9
tanvirahmedme <tanvir09k3@gmail.com>
2026-05-27 13:37:11 +0000

    Deploying to gh-pages from @ tanvirahmedme/tanvirahmedme.github.io@360b80decc19df3313571a2dcb3d813190efdadc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,11 +420,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-05-21-txst-social-feature/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
